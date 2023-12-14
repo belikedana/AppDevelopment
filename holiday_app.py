9b685fa3d360b2c904beefc801da65c1b13ca2fd
Dana  Schön <caity@Danas-MacBook-Air.local>
2023-12-14 14:37:03 +0100

    updated code

--- holiday_app.py
+++ holiday_app.py
@@ -10,4 +10,5 @@ st.set_page_config(
 st.header('Happy Holidays', divider='rainbow')
 
 # add an image
-st.image('images/snowman.webp', caption='its a snowman')
+file_path = "https://raw.githubusercontent.com/belikedana/AppDevelopment/main/images/snowman.webp"
+st.image(file_path, caption='its a snowman')
